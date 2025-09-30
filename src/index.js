491404c7fdb9864056458f880c8a52e70511dbde
MuhammedZeki <muhammed.codes@gmail.com>
2025-09-30 18:25:44 +0300

    İmg class özeliği eklendi

--- src/index.js
+++ src/index.js
@@ -7,3 +7,64 @@ import './less/index.less'
 
 
 // Kodlar buraya gelecek!
+const imgs = document.querySelectorAll('img');
+imgs.forEach((img) => {
+  img.addEventListener('mouseenter', (e) => {
+    img.classList.add('grayscale');
+    e.preventDefault();
+  });
+  img.addEventListener('mouseleave', (e) => {
+    img.classList.remove('grayscale');
+    e.preventDefault();
+  });
+});
+
+const body = document.body;
+window.addEventListener('keydown', (e) => {
+  if (e.key === '1') {
+    body.classList.add('theme1');
+    body.classList.remove('theme2', 'theme3');
+  } else if (e.key === '2') {
+    body.classList.add('theme2');
+    body.classList.remove('theme1', 'theme3');
+  } else if (e.key === '3') {
+    body.classList.add('theme3');
+    body.classList.remove('theme1', 'theme2');
+  } else if (e.key === 'Escape') {
+    body.classList.remove('theme1', 'theme2', 'theme3');
+  }
+});
+
+const button = document.querySelector('form button');
+const input = document.querySelector('form input');
+
+input.addEventListener('input', (e) => {
+  e.target.value = e.target.value.toUpperCase();
+  e.target.value.length < 5
+    ? (button.disabled = true)
+    : (button.disabled = false);
+
+  e.preventDefault();
+});
+
+const prap = document.querySelector('#submitResult');
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+  const msg = input.value;
+  prap.textContent = `${msg} başarı ile kaydedildi...`;
+  input.value = '';
+  button.disabled = true;
+  e.preventDefault();
+});
+
+const a = document.querySelectorAll(".nav a");
+a.forEach((item)=>{
+    item.addEventListener("mouseenter",(e)=>{
+        e.preventDefault();
+        item.classList.add("underline");
+    })
+    item.addEventListener("mouseleave", (e) => {
+        e.preventDefault();
+        item.classList.remove("underline");
+    });
+})
